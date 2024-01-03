54501e5ad8e788ca84a8bb3ee2a0ea97f6eaa56a
Djair <djairdecastrofeitosa@gmail.com>
2024-01-02 22:29:32 -0300

    first commit

---
+++ Aula 4hrs - JS.js
@@ -0,0 +1,9 @@
+let nome = "castro"; // tipo string
+
+let numero = 15; // tipo número literal
+
+let sim_nao = true; // valores booleanos(só recebem TRUE ou FALSE)
+
+let indefinido = undefined; // algo que é indefinido
+
+let campoDeSeleção = null; // para redefinir um valor
